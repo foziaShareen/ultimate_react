09b630c2a2ba62f306ec40d1b3fedf544a0cac11
foziaShareen <66553473+foziaShareen@users.noreply.github.com>
2021-03-28 10:40:11 +0500

    add card in react

---
+++ Sdata.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+let Sdata =[
+    {
+        imgsrc:"https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
+        title:"image",
+        surname:"scene"
+    },
+    {
+        imgsrc:"https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
+        title:"image",
+        surname:"scene"
+    },
+    {
+        imgsrc:"https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
+        title:"image",
+        surname:"scene"
+    }
+]
+export default Sdata
